d10377c37f6be0f1eb6ae257ae9aa75629254f2d
ChengYuchao <3414906693@qq.com>
2026-01-18 18:03:15 +0800

    [feat]:MCP服务搭建

---
+++ mcp-server/internal/router/mcp.go
@@ -0,0 +1,36 @@
+package router
+
+import (
+	"core/ai/tools"
+	"mcp-server/internal/tool"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
+)
+
+type McpRouter struct {
+}
+
+func (u *McpRouter) Register(engine *gin.Engine) {
+	//需要两个 /sse， /message
+	//这里需要使用mcp-go 创建mcp服务
+	mcpServer := server.NewMCPServer(
+		"faberAI mcp server",
+		mcp.LATEST_PROTOCOL_VERSION,
+		server.WithToolCapabilities(true),
+		server.WithResourceCapabilities(true, true),
+		server.WithPromptCapabilities(true),
+	)
+	weather := tool.NewWeatherTool(tools.ApiKey)
+	mcpServer.AddTool(weather.Build(), weather.Invoke)
+	sseServer := server.NewSSEServer(
+		mcpServer,
+		server.WithBaseURL("http://localhost:7777"),
+		server.WithSSEEndpoint("/sse"),
+		server.WithMessageEndpoint("/message"),
+		server.WithKeepAlive(true),
+	)
+	engine.GET("/sse", gin.WrapH(sseServer.SSEHandler()))
+	engine.POST("/message", gin.WrapH(sseServer.MessageHandler()))
+}
